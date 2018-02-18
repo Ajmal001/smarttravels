005f619b793692d4c72043fda2495fe929a92536
shakilzaman87 <w3schools000@gmail.com>
2018-02-18 16:39:02 +0600

    Route changed

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('somethings', function(){
+
+Route::get('somethingnew', function(){
 
 });
+
+
 /*
 ================================
 Front End Routes
